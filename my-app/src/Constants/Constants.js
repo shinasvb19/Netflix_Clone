26a1710337c885c4e780abbfce6274b91ee55cd4
shinasvb19 <myoffcial.shinas@gmail.com>
2022-11-23 12:12:22 +0530

    INITIAL

--- my-app/src/Constants/Constants.js
+++ my-app/src/Constants/Constants.js
@@ -1,3 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
-export const API_KEY = "43cccc08911c233ef01b8c57ae605ad9";
+export const API_KEY = "cb5205554db9f6dc62f13146d9f05d82";
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
